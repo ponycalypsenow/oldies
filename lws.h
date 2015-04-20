e5fe661b366ef5b760c31ef880789dc1552934d2
ponycalypsenow <ponycalypsenow@gmail.com>
2015-04-20 08:43:12 +0200

    Create lws.h

---
+++ lws.h
@@ -0,0 +1,20 @@
+#ifndef LWSH
+#define LWSH
+
+#include "stdafx.h"
+#include "render.h"
+#include "keyframer.h"
+#include "lwo.h"
+
+/*
+
+  lws loader
+
+*/
+
+class LWS:public SCENE{
+public:
+	LWS(char *);
+};
+
+#endif
