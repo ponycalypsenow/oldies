2cf118d58d393334407f998a06dad2a61704ba76
ponycalypsenow <ponycalypsenow@gmail.com>
2015-04-20 08:45:26 +0200

    Create time.h

---
+++ time.h
@@ -0,0 +1,21 @@
+#ifndef TIMEH
+#define TIMEH
+
+#include "stdafx.h"
+
+/*
+
+  time
+
+*/
+
+class TIME{
+public:
+	float frame;
+	float scene;
+
+	TIME(void);
+	void Get(void);
+};
+
+#endif TIMEH
